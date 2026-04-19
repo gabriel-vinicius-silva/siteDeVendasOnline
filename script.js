816d65af691bc5559bb6bc7fd2ee2a877322a345
gabriel-vinicius-silva <gabrieuvinici@gmail.com>
2026-04-19 16:29:37 -0300

    fluindo

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let carrinho = {};
 
 function add(nome, preco) {
   if (!carrinho[nome]) {
-    carrinho[nome] = {qtd: 0, preco: preco};
+    carrinho[nome] = {
+      qtd: 0,
+      preco: preco
+    };
   }
+
   carrinho[nome].qtd++;
   atualizar();
 }
@@ -11,59 +15,126 @@ function add(nome, preco) {
 function remover(nome) {
   if (carrinho[nome] && carrinho[nome].qtd > 0) {
     carrinho[nome].qtd--;
+
+    if (carrinho[nome].qtd <= 0) {
+      delete carrinho[nome];
+    }
   }
+
   atualizar();
 }
 
+function formatarNome(nome) {
+  const nomes = {
+    Hamburguer: "Hambúrguer",
+    XBurguer: "X-Burguer",
+    Bauru: "Bauru",
+    XTudo: "X-Tudo",
+    XBacon: "X-Bacon",
+    Misto: "Misto",
+    FileCarne: "Filé de Carne",
+    Sertanejo: "Sertanejo",
+    FileSol: "Filé de Sol",
+
+    Buiu: "Buiu",
+    XCaseiro: "X-Caseiro",
+    BuiuEspecial: "Buiu Especial",
+
+    Frangabacon: "Frangabacon",
+    Franburguer: "Franburguer",
+    FranCoracao: "Fran Coração",
+    Passaporte: "Passaporte",
+    BuiuDuplo: "Buiu Duplo",
+    XCalabresa: "X-Calabresa",
+    BuiuFrango: "Buiu Duplo c/ Frango",
+
+    XCamarao: "X-Camarão",
+    Framarao: "Framarão",
+
+    Tradicional: "Tradicional",
+    Especial: "Especial",
+    HotSertanejo: "Sertanejo",
+    HotCoracao: "De Coração",
+    HotCamarao: "De Camarão",
+    HotBacon: "De Bacon",
+    SandFrango: "Sanduíche de Frango",
+
+    BatataP: "Batata Pequena",
+    BatataM: "Batata Média",
+    BatataCheddar: "Batata + Cheddar + Bacon",
+    FileFritas: "Filé c/ Fritas",
+    CalabresaFritas: "Calabresa c/ Fritas",
+    AceboladaFritas: "Acebolada c/ Fritas",
+
+    PastelQueijo: "Pastel Queijo",
+    PastelMussarela: "Pastel Mussarela",
+    PastelCarneSol: "Pastel Carne de Sol",
+    PastelChocolate: "Pastel Chocolate",
+    PastelFrango: "Pastel Frango",
+    PastelPizza: "Pastel Pizza",
+
+    RefriLata: "Refri Lata",
+    Refri250: "Refri 250ml",
+    Refri600: "Refri 600ml",
+    Refri1L: "Refri 1L",
+    Coca1L: "Coca 1L",
+    AguaGas: "Água c/ Gás",
+    Agua: "Água Mineral",
+
+    SucoAgua: "Suco na Água",
+    SucoLeite: "Suco no Leite",
+
+    Acai400: "Açaí 400ml",
+    Acai500: "Açaí 500ml",
+    Guarana: "Guaraná do Amazonas 400ml",
+
+    ShakeMorango: "Milk Shake Morango",
+    ShakeChocolate: "Milk Shake Chocolate",
+    ShakeOvomaltine: "Milk Shake Ovomaltine"
+  };
+
+  return nomes[nome] || nome;
+}
+
 function atualizar() {
-  let lista = document.getElementById("lista");
+  const lista = document.getElementById("lista");
+  const totalSpan = document.getElementById("total");
+
   let total = 0;
   lista.innerHTML = "";
 
   for (let item in carrinho) {
-    if (carrinho[item].qtd > 0) {
-      let subtotal = carrinho[item].qtd * carrinho[item].preco;
-      total += subtotal;
+    const qtd = carrinho[item].qtd;
+    const preco = carrinho[item].preco;
+    const subtotal = qtd * preco;
 
-      let nomeExibido = "";
-      if(item === "XCamarao") nomeExibido = "X Camarão";
-      if(item === "FrangoCoracao") nomeExibido = "Frango com Coração";
-      if(item === "XCaseiro") nomeExibido = "X Caseiro";
-      if(item === "MegaBuiu") nomeExibido = "Mega Buiu Especial";
+    total += subtotal;
 
-      lista.innerHTML += `
-        <div class="item-carrinho">
-          ${nomeExibido} x${carrinho[item].qtd} - R$${subtotal}
-        </div>
-      `;
-
-      document.getElementById("qtd-" + item).innerText = carrinho[item].qtd;
-    } else {
-      document.getElementById("qtd-" + item).innerText = 0;
-    }
+    lista.innerHTML += `
+      <div class="item-carrinho">
+        ${formatarNome(item)} x${qtd} - R$ ${subtotal.toFixed(2)}
+      </div>
+    `;
   }
 
-  document.getElementById("total").innerText = total;
+  totalSpan.innerText = total.toFixed(2);
 }
 
 function enviar() {
-  let mensagem = "Olá, quero fazer um pedido:%0A";
-
-  for (let item in carrinho) {
-    if (carrinho[item].qtd > 0) {
-      let nomeExibido = "";
-      if(item === "XCamarao") nomeExibido = "X Camarão";
-      if(item === "FrangoCoracao") nomeExibido = "Frango com Coração";
-      if(item === "XCaseiro") nomeExibido = "X Caseiro";
-      if(item === "MegaBuiu") nomeExibido = "Mega Buiu Especial";
-
-      mensagem += `- ${nomeExibido} x${carrinho[item].qtd}%0A`;
-    }
+  if (Object.keys(carrinho).length === 0) {
+    alert("Adicione itens ao pedido.");
+    return;
   }
 
-  let total = document.getElementById("total").innerText;
-  mensagem += "%0ATotal: R$" + total;
+  let mensagem = "Olá, quero fazer um pedido:%0A%0A";
 
-  let url = "https://wa.me/5584991139970?text=" + mensagem;
+  for (let item in carrinho) {
+    mensagem += `• ${formatarNome(item)} x${carrinho[item].qtd}%0A`;
+  }
+
+  mensagem += `%0ATotal: R$ ${document.getElementById("total").innerText}`;
+
+  const url = "https://wa.me/5584988687921?text=" + mensagem;
+
   window.open(url, "_blank");
 }
